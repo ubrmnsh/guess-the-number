689e497ad6535b91fa5c2cc0777fdca08f7a867c
ubrmnz <zxvf@tutanota.com>
2022-08-08 00:25:51 +0530

    feat: completed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("guess a number!");
-    println!("please input your guess");
+    println!("Guess a Number!\n");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line.");
+    //println!("The secret number is: {secret_number}\n");
 
-    println!("You guessed: {guess}")
+    loop {
+        println!("Please input your guess :\n");
+
+        // variables are immutable by default in Rust, 'mut' keyword is used to make it mutable
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.\n");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}\n");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small\n"),
+            Ordering::Greater => println!("Too big\n"),
+            Ordering::Equal => {
+                println!("You win!\n");
+                break;
+            }
+        }
+    }
 }
